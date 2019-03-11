8ff73c09e1f2dd1fa5ae7142b9c2fc732846e2d2
acoshift <acoshift@gmail.com>
2019-03-11 21:18:20 +0700

    adjust comment

--- auth/main.go
+++ auth/main.go
@@ -91,11 +91,11 @@ func (h *auth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// end: CSRF protection
 
 	// start: CORS
+	// allow only http://localhost:8080
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 	if r.Method == http.MethodOptions {
-		// allow only http://localhost:8080
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Requested-With")
 		w.Header().Set("Access-Control-Max-Age", "3600")
